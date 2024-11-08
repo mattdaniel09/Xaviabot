82f029a0859002ad8705a834e1ce1cde7832dea1
Kupalpriv <churchillabing100@gmail.com>
2024-11-08 21:14:39 +0800

    Update showPrefix.js

--- plugins/onMessage/showPrefix.js
+++ plugins/onMessage/showPrefix.js
@@ -1,15 +1,22 @@
 const langData = {
     "en_US": {
-        "prefix.get": "🌐 | The bot's current prefix is [ {prefix} ].\nType !help to see all commands."
+        "prefix.get": "🌐 | The bot's current prefix is [ {prefix} ].\nType {prefix}help to see all commands.",
+        "noPrefix": "🌐 | No prefix is currently set. You can use commands without a prefix."
     }
 };
 
 function onCall({ message, getLang, data }) {
     const validTriggers = ["prefix", "Prefix"];
 
+    // Check if prefix is set in thread data or config, fallback to no prefix message
+    const prefix = data?.thread?.data?.prefix || global.config.PREFIX || "";
+
     if (validTriggers.includes(message.body) && message.senderID !== global.botID) {
-        const prefix = data?.thread?.data?.prefix || global.config.PREFIX;
-        message.reply(getLang("prefix.get", { prefix }));
+        if (prefix) {
+            message.reply(getLang("prefix.get", { prefix }));
+        } else {
+            message.reply(getLang("noPrefix"));
+        }
     }
 
     return;
