41078220b1531d9674b2512e815f5d56184cec81
Kupalpriv <churchillabing100@gmail.com>
2024-11-08 16:55:12 +0800

    Update api.js

--- plugins/commands/api/api.js
+++ plugins/commands/api/api.js
@@ -1,9 +1,8 @@
-// api.js
-module.exports = {
+// api.js (ES module)
+export default {
     markApi: 'https://markdevs-last-api-vtjp.onrender.com',
     josh: 'https://joshweb.click',
     canvas: 'https://api-canvass.vercel.app',
     jonel: 'https://appjonellccapis.zapto.org',
-    flux: 'https://ccprojectapis.ddns.net/api/flux', // Adding the flux API endpoint for image generation
+    flux: 'https://ccprojectapis.ddns.net/api/flux',
 };
-
