2028d9ba4b0e447fb8de587394d366dee5594f69
Kupalpriv <churchillabing100@gmail.com>
2024-11-08 17:03:01 +0800

    Update api.js

--- plugins/commands/api/api.js
+++ plugins/commands/api/api.js
@@ -3,6 +3,5 @@ export default {
     markApi: 'https://markdevs-last-api-vtjp.onrender.com',
     josh: 'https://joshweb.click',
     canvas: 'https://api-canvass.vercel.app',
-    jonel: 'https://appjonellccapis.zapto.org',
-    flux: 'https://ccprojectapis.ddns.net/api/flux',
+    jonel: 'https://ccprojectapis.ddns.net',
 };
