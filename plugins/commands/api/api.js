a0cc2564c2f54f4ccac4ec604cd6d01293cf5a07
Kupalpriv <churchillabing100@gmail.com>
2024-11-29 09:41:00 +0800

    Update api.js

--- plugins/commands/api/api.js
+++ plugins/commands/api/api.js
@@ -3,6 +3,7 @@ export default {
     neth: 'https://nethwieginedev.vercel.app',
     mark: 'https://markdevs-last-api-vtjp.onrender.com',
     josh: 'https://joshweb.click',
+    kaizen: 'https://kaiz-apis.gleeze.com',
     kenlie: 'https://api.kenliejugarap.com',
     canvas: 'https://api-canvass.vercel.app',
     jonel: 'https://ccprojectapis.ddns.net',
